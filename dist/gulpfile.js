0c37d5d7e22bf518d79616230ca267d794d1149e
bhenderson71 <bhenderson71@gmail.com>
2017-01-13 06:09:40 -0600

    Add 'dist/' from commit '2074cbec67fdc3078ecd19edacb1aef5a441b16d'
    git-subtree-dir: dist
    git-subtree-mainline: 2074cbec67fdc3078ecd19edacb1aef5a441b16d
    git-subtree-split: 2074cbec67fdc3078ecd19edacb1aef5a441b16d

---
+++ dist/gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const pug = require('gulp-pug');
+const sass = require('gulp-ruby-sass');
+const del = require('del');
+const livereload = require('gulp-livereload');
+const paths = {
+        pug: ['src'],
+        style_sass: 'src/sass'
+    };
+const LOCAL_SERVER_PORT = 4000;
+
+
+gulp.task('server', () => 
+    connect.server({
+        port: LOCAL_SERVER_PORT,
+        root: './dist',
+        livereload: true
+    })
+);
+
+gulp.task('html', () =>
+    gulp.src('src/**/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('styles', () =>
+        sass('./src/**/*.scss')
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('watch', function(){
+    livereload.listen({
+        start: true,
+        reloadPage: 'index.html'
+    })
+    gulp.watch(paths.pug + '*.pug', ['pug:watch'])
+    gulp.watch(paths.style_sass + '*.scss', ['sass:watch'])
+});
+
+gulp.task('pug:watch', () => 
+    gulp.src('src/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('sass:watch', () =>
+    gulp.src('src/styles/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/styles'))
+);
+
+gulp.task('dev', ['clean', 'html', 'styles', 'server', 'watch']);
+
+gulp.task('default', ['dev']);
+
+gulp.task('clean', function(){
+    return del([
+    'dist/**/*',
+  ]);
+});
